4b7a32ec537af7b09d70c5ac2d522c82ca365bf9
oushneek <oushneek@gmail.com>
2016-12-24 21:17:07 +0600

    course model created

---
+++ app/models/Course.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Course extends Eloquent{
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'courses';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+}
